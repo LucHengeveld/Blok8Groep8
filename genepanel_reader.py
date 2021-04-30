3d88370246796ffe2fd844be1a87a8a9c2fb8da6
[MargoRaijmakers] <margoraijmakers@gmail.com>
2021-04-30 20:38:02 +0200

    make_genepanel_list_set toegevoegd

--- genepanel_reader.py
+++ genepanel_reader.py
@@ -3,6 +3,7 @@ Margo Raijmakers
 28-04-2021
 """
 import pandas as pd
+import re
 
 
 def excel_reader(file_name):
@@ -26,12 +27,35 @@ def get_column(gp_table, column_name):
     return gp_table[column_name].tolist()
 
 
+def make_genepanel_list_set(gene_panels_list):
+    """This function makes a list and set of all the gene panels with
+    the (AD, etc.) removed.
+
+    :param gene_panels_list: the values from a column in a list
+    :return: the list and set of all the gene panels
+    """
+    gps_list = []
+    gps_set = []
+    for gene_panels in gene_panels_list:
+        gp_list = []
+        gp_list.clear()
+        for gene_panel in re.sub('[a-zA-Z]+;', ",", gene_panels).split(";"):
+            gp = re.sub('( (.*))', "", gene_panel)
+            gp_list.append(gp)
+            if gp in gps_set:
+                pass
+            else:
+                gps_set.append(gp)
+        gps_list.append(gp_list[:])
+    return gps_list, gps_set
+
+
 def main():
     file_name = "GenPanelOverzicht_DG-3.1.0_HAN.xlsx"
     gp_table = excel_reader(file_name)
-    genes = get_column(gp_table, 'GenePanels_Symbol')
-    gene_panels = get_column(gp_table, 'GenePanel')
-    print(len(genes))
-    print(len(gene_panels))
+    genes = get_column(gp_table, "GenePanels_Symbol")
+    gene_panels_list = get_column(gp_table, "GenePanel")
+    gps_list, gps_set = make_genepanel_list_set(gene_panels_list)
+
 
 main()
