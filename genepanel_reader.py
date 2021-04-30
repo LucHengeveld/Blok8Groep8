1a8eb3352481a2645540b33d0572b62acc29e5de
[MargoRaijmakers] <margoraijmakers@gmail.com>
2021-04-30 17:26:37 +0200

    get_column toegevoegd

--- genepanel_reader.py
+++ genepanel_reader.py
@@ -10,15 +10,28 @@ def excel_reader(file_name):
     Python.
 
     :param file_name: the name of the file
-    :return: the table
+    :return: the gene panels table
     """
     return pd.read_excel(file_name)
+
+
+def get_column(gp_table, column_name):
+    """This function gets the values from a column from the gene panels
+    table and returns it in a list.
+
+    :param gp_table: the gene panels table
+    :param column_name: the name of the column
+    :return: the values from a column in a list
+    """
+    return gp_table[column_name].tolist()
 
 
 def main():
     file_name = "GenPanelOverzicht_DG-3.1.0_HAN.xlsx"
     gp_table = excel_reader(file_name)
-    print(gp_table['GenePanels_Symbol'].tolist())
-
+    genes = get_column(gp_table, 'GenePanels_Symbol')
+    gene_panels = get_column(gp_table, 'GenePanel')
+    print(len(genes))
+    print(len(gene_panels))
 
 main()
